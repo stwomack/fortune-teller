33e1ed77fab8901878cd8fdacd8da15c81c59225
Steve Womack <stwomack@gmail.com>
2016-07-27 17:46:35 -0500

    yolo

--- scripts/create_services_pcf.sh
+++ scripts/create_services_pcf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cf cs p-mysql 100mb-dev fortunes-db
-cf cs p-config-server standard config-server -c '{"git": { "uri": "https://github.com/spring-cloud-samples/fortune-teller", "searchPaths": "configuration" } }'
-cf cs p-service-registry standard service-registry
-cf cs p-circuit-breaker-dashboard standard circuit-breaker-dashboard
+cf cs p-config-server standard womack-config-server -c '{"git": { "uri": "https://github.com/spring-cloud-samples/fortune-teller", "searchPaths": "configuration" } }'
+cf cs p-service-registry standard womack-service-registry
+cf cs p-circuit-breaker-dashboard standard womack-circuit-breaker-dashboard
